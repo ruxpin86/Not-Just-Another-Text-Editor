f67cb46a3f608f66b076cc95781799bb7111c24b
William Glynn <teddy_gram@Williams-Mac-mini.local>
2022-07-20 14:03:16 -0600

    heroku is so cool

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack. **
@@ -23,6 +24,8 @@ module.exports = () => {
         template: "./index.html",
         title: "JATE",
       }),
+
+      new WorkboxPlugin.GenerateSW(),
 
       new InjectManifest({
         swSrc: "./src-sw.js",
