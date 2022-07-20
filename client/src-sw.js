48572d2a242215136f7a90686e32f00d45e53702
William Glynn <teddy_gram@Williams-Mac-mini.local>
2022-07-20 13:30:55 -0600

    fixed indexDB so text persists in the editor

--- client/src-sw.js
+++ client/src-sw.js
@@ -39,3 +39,21 @@ registerRoute(
     ],
   })
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("./src-sw.js").then(
+      function (registration) {
+        // Registration was successful
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function (err) {
+        // registration failed :(
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
